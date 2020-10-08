6ad407c6336422e164941eb9ab249ded869818bc
Ramon <juanramoncarceles@gmail.com>
2020-10-08 18:42:18 +0200

    Initial commit.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Tibidabo`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Asuni`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,8 +15,23 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-sharp`,
+      options: {
+        checkSupportedExtensions: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Available options and their defaults:
+        base64Width: 20,
+        forceBase64Format: ``, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -25,6 +42,31 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-wordpress-experimental`,
+      options: {
+        url:
+          process.env.WPGRAPHQL_URL,
+        schema: {
+          // Prefixes all WP Types with "Wp" so "Post and allPost" become "WpPost and allWpPost".
+          typePrefix: `Wp`,
+        },
+        develop: {
+          // Caches media files outside of Gatsby's default cache an thus allows them to persist through a cache reset.
+          hardCacheMediaFiles: true,
+        },
+        type: {
+          Post: {
+            limit:
+              process.env.NODE_ENV === `development`
+                ? // Lets just pull 50 posts in development to make it easy on ourselves (aka. faster).
+                  50
+                : // and we don't actually need more than 5000 in production for this particular site
+                  5000,
+          },
+        },
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/templates/post.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+export default function Post({ pageContext }) {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>{pageContext.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: pageContext.content }} />
+    </Layout>
+  );
+}
