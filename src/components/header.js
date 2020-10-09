6f7fb898d7c4babb29dd37b605640093e47a5d5c
Ramon <juanramoncarceles@gmail.com>
2020-10-09 18:37:07 +0200

    Created menu link from the main WP menu data.

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,63 @@
-import { Link } from "gatsby";
-import PropTypes from "prop-types";
 import React from "react";
+import PropTypes from "prop-types";
+import { Link, useStaticQuery, graphql } from "gatsby";
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+const Header = ({ siteTitle }) => {
+  // Consider getting the menu id dynamically from context.
+  const {
+    wpMenu: {
+      menuItems: { nodes },
+    },
+  } = useStaticQuery(graphql`
+    query MainMenuQuery {
+      wpMenu(id: { eq: "dGVybToy" }) {
+        menuItems {
+          nodes {
+            path
+            label
+            id
+          }
+        }
+      }
+    }
+  `);
+
+  console.log(nodes);
+
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-);
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+        {nodes.map((node) => (
+          <Link to={node.path} key={node.id}>
+            {node.label}
+          </Link>
+        ))}
+      </div>
+    </header>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
