886eae359a561d9bb864e4a5fc1b828c91a9c1bb
Ramon <juanramoncarceles@gmail.com>
2020-10-14 16:37:32 +0200

    Added TODO comment.

--- src/templates/post.js
+++ src/templates/post.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 export default function Post({ pageContext }) {
+  // TODO: Use data instead ?
   return (
     <Layout>
       <SEO title="Home" />
