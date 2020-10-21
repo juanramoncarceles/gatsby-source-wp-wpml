2cd67b57fa73cdd6d54e5bb95efde5a7a595d812
Ramon <juanramoncarceles@gmail.com>
2020-10-21 16:21:35 +0200

    Added comment on the Footer to indicate which contents may include.

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,3 +10,22 @@ const Footer = ({ siteTitle }) => (
 );
 
 export default Footer;
+
+/*
+
+Contents of the footer may include:
+
+Links to this pages:
+- Legal
+- About us
+- The Team
+- Subscribe to Newsletter
+
+Also this content of the brand:
+  Asuni logo and "RhinoToolKit is an Asuni brand"
+
+Also maybe links to social media...
+
+All this could be done in here or using ACF with an options page in WP.
+
+*/
